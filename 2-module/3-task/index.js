eef1ecdfce487f11da603f03fb9948bd6a250fe9
Daniil Kim <justpotato13@gmail.com>
2021-12-23 17:10:16 +0300

    Решил задачки к занятию №2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
-let calculator = {
-  // ваш код
-};
+var calculator = {
+  read: function(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum: function() {
+    return this.a + this.b;
+  },
+
+  mul: function() {
+    return this.a * this.b;
+  },
+}
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
